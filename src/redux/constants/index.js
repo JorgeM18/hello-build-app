39b5eb0347caaa4be63c947dbe8b2806198360e0
Malo <Jorge.Malo@Outsourcing.Libertycolombia.com>
2021-05-05 23:00:38 -0500

    Deleted env file

---
+++ src/redux/constants/index.js
@@ -0,0 +1,5 @@
+export const CREATE_USER = 'CREATE_USER';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const GET_REPOS = 'GET_REPOS';
+export const GET_EVENTS = 'GET_EVENTS';
